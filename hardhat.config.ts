8aec1e72a92b28c60d234251490a437a97684ab4
Bang Nguyen <bangdtbk@gmail.com>
2023-06-27 10:54:07 +0700

    successfully test Box

---
+++ hardhat.config.ts
@@ -0,0 +1,33 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import '@openzeppelin/hardhat-upgrades';
+const { mnemonic} = require('./secrets.json');
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    binanceTestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545", 
+      accounts: {mnemonic: mnemonic}, 
+}
+  },
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
+}
